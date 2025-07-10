8712122fd167f17cf5feeb7b04fceae56640b925
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-07-10 18:07:14 +0330

    html part

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar Project</title>
+    <meta name="description", content="My Own Calendar Project">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Course Calendar <br> My Calendar Project</h1>
+    </header>
+
+    <!-- Clock -->
+     <div class="clock-container">
+        <div id="clock"></div>
+     </div>
+
+     <!-- Calendar Section -->
+      <div class="calendar">
+        <div class="nav-btn-container">
+            <button class="nav-btn">(<<)</button>
+            <h2 id="monthYear" style="margin:0"></h2>
+            <button class="nav-btn">(>>)</button>
+        </div>
+
+        <div class="calendar-grid" id="calendar"></div>
+
+      </div>
+
+      <!-- Modal for add/edit/delete appoinment -->
+       <div class="modal">
+        <div class="modal-content">
+       <div id="eventSelectorWrapper">
+        <label for="eventSelector">
+            <strong>Select Event:</strong>
+        </label>
+        <select id="eventSelector">
+            <option disabled selected>Choose Event...</option>
+        </select>
+       </div>
+
+       <!-- Main Form -->
+        <form method="POST" id="eventForm">
+            <input type="hidden" name="action" id="formAction" value="add">
+            <input type="hidden" name="event_id" id="eventId">
+
+            <label for="courseName">Course Title:</label>
+            <input type="text" name="course_name" id="courseName" required>
+
+            <label for="instructorName">Instructor Name:</label>
+            <input type="text" name="instructor_name" id="instructorName" required>
+
+            <label for="startDate">Start Date:</label>
+            <input type="date" name="start_date" id="startDate" required>
+
+            <label for="endDate">End Date:</label>
+            <input type="date" name="end_date" id="endDate" required>
+
+            <button type="submit">Save</button>
+        </form> 
+
+        <!-- Delete Form -->
+         <form method="POST" onsubmit="return confirm('Are you sure you want to delete this appoinment?')">
+            <input type="hidden" name="action" value="delete">
+            <input type="hidden" name="event_id" id="deleteEventId">
+            <button type="submit" class="submit-btn">Delete</button>
+         </form>
+
+         <!-- Cancel -->
+          <button type="button" class="submit-btn">Cancel</button>
+        </div>
+       </div>
+          <script src="calendar.js"></script>
+</body>
+</html>
