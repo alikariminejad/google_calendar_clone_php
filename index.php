40a166985707f6abd7bbfdc44486e51ab3a97597
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-07-10 19:44:14 +0330

    css part

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar Project</title>
     <meta name="description", content="My Own Calendar Project">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=inter:wght@400;600;700&display=swap"">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
